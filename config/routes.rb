77f5f36183feaf85777341a0234119d95a231d78
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-10 16:10:28 -0700

    add welcome route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "/"
+  root "welcome#index"
 
   resources :merchants, only: [] do
     member do
