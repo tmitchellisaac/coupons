87fec560192fbacedc36df3f1dc96e4537f4bb0b
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-10 16:04:22 -0700

    Add root "/"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "/"
 
   resources :merchants, only: [] do
     member do
