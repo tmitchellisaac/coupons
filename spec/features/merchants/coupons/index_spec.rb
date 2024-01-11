14b6e00c639fd7cb2954959110c0833686dbe782
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-11 16:46:13 -0700

    Change some test data and fix rails_helper call

--- spec/features/merchants/coupons/index_spec.rb
+++ spec/features/merchants/coupons/index_spec.rb
@@ -1,11 +1,11 @@
-require "spec_helper"
+require "rails_helper"
 
 describe "Coupons" do
   describe "index page" do
     merchant_1 = Merchant.create!(name: "Walmart", status: :enabled)
-    coupon_1 = merchant_1.coupons.create(name: "15% off", uniq_code: "15%OFF", amt_off: 15, dollar_or_percent: 1)
-    coupon_2 = merchant_1.coupons.create(name: "5_dollars_off", uniq_code: "$5OFF", amt_off: 5, dollar_or_percent: 0)
-    coupon_3 = merchant_1.coupons.create(name: "10_dollars_off", uniq_code: "$10OFF", amt_off: 10, dollar_or_percent: 0)
+    coupon_1 = merchant_1.coupons.create(name: "15% off", uniq_code: "15%OFF", amt_off: 15, dollar_or_percent: "%")
+    coupon_2 = merchant_1.coupons.create(name: "5_dollars_off", uniq_code: "$5OFF", amt_off: 5, dollar_or_percent: "$")
+    coupon_3 = merchant_1.coupons.create(name: "10_dollars_off", uniq_code: "$10OFF", amt_off: 10, dollar_or_percent: "$")
 
     it "has an index page displaying all coupons for that merchant" do
     visit "/merchants/#{merchant_1.id}/dashboard"
@@ -18,8 +18,8 @@ describe "Coupons" do
     expect(page).to have_content("10_dollars_off")
 
     expect(page).to have_content("15%")
-    expect(page).to have_content("$5")
-    expect(page).to have_content("$15")
+    expect(page).to have_content("5$")
+    expect(page).to have_content("10$")
 
     click_link "15% off"
     expect(current_path).to eq(("/merchants/#{merchant_1.id}/coupons/#{coupon_1.id}"))
