853e9cb7869028f1c583964d425d108d49a58b0c
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-12 15:38:19 -0700

    Add test for sad path coupon creation

--- spec/features/merchants/coupons/new_spec.rb
+++ spec/features/merchants/coupons/new_spec.rb
@@ -23,6 +23,28 @@ describe "Coupons" do
       expect(page).to have_content("$20 off")
       expect(page).to have_content("20$")
       expect(page).to have_content("qwer")
+      expect(Coupon.all.count).to eq 5
+    end
+
+    it "has a sad path for coupon creation if all required info not submitted" do
+      merchant_1 = Merchant.create!(name: "Walmart", status: :enabled)
+      coupon_1 = merchant_1.coupons.create!(name: "15% off", uniq_code: "x95l", amt_off: 15, dollar_or_percent: "%", status: 1)
+      coupon_2 = merchant_1.coupons.create!(name: "5_dollars_off", uniq_code: "ikm", amt_off: 5, dollar_or_percent: "$", status: 1)
+      coupon_3 = merchant_1.coupons.create!(name: "10_dollars_off", uniq_code: "qaz", amt_off: 10, dollar_or_percent: "$", status: 1)
+      coupon_4 = merchant_1.coupons.create!(name: "4_dollars_off", uniq_code: "wsx", amt_off: 4, dollar_or_percent: "$", status: 1)
+      
+      visit "/merchants/#{merchant_1.id}/coupons"
+      click_link "New Coupon"
+      expect(current_path).to eq("/merchants/#{merchant_1.id}/coupons/new")
+
+      fill_in(:name, with: "$20 off")
+      fill_in(:dollar_or_percent, with:"$")
+      fill_in(:uniq_code, with:"qwer")
+      click_button "Create New Coupon"
+
+      expect(current_path).to eq("/merchants/#{merchant_1.id}/coupons/new")
+      expect(page).to have_content("Not enough information to create, try again")
+      expect(Coupon.all.count).to eq 4
     end
   end
 end
