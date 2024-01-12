3982c34b0676edc29013aeac298c00fef0c6a114
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-12 15:19:52 -0700

    Add validations for uniq code and limit of 5 active coupons per merchant

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,8 +1,30 @@
 class Coupon < ApplicationRecord
   belongs_to :merchant
+  has_many :invoices
+  
   validates :name, presence: true
+  validates :uniq_code, uniqueness: true
   validates :uniq_code, presence: true
+
   validates :amt_off, presence: true
   validates :dollar_or_percent, presence: true
+  validates :status, presence: true
+
+ 
+  validate :limit_inactive_coupons, on: :create
+
+  enum status: {inactive: 0, active: 1}
+
+  private
+
+  def limit_inactive_coupons
+    max_inactive_coupons = 5 
+    return unless merchant.present?
+    if merchant.coupons.where(status: 1).count >= max_inactive_coupons
+      errors.add(:base, "Exceeded the limit of inactive coupons")
+    end
+  end
+
+
 
 end
