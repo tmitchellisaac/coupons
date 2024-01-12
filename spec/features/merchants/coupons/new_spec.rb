aad06a73c8454542a56ee2bea2fa47902b71efa9
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-12 15:46:32 -0700

    Add test for too many coupons created per merchant

--- spec/features/merchants/coupons/new_spec.rb
+++ spec/features/merchants/coupons/new_spec.rb
@@ -46,5 +46,23 @@ describe "Coupons" do
       expect(page).to have_content("Not enough information to create, try again")
       expect(Coupon.all.count).to eq 4
     end
+
+    it "has a sad path for if more than 5 coupons are attempted to be created" do
+      merchant_1 = Merchant.create!(name: "Walmart", status: :enabled)
+      coupon_1 = merchant_1.coupons.create!(name: "15% off", uniq_code: "x95l", amt_off: 15, dollar_or_percent: "%", status: 1)
+      coupon_2 = merchant_1.coupons.create!(name: "5_dollars_off", uniq_code: "ikm", amt_off: 5, dollar_or_percent: "$", status: 1)
+      coupon_3 = merchant_1.coupons.create!(name: "10_dollars_off", uniq_code: "qaz", amt_off: 10, dollar_or_percent: "$", status: 1)
+      coupon_4 = merchant_1.coupons.create!(name: "4_dollars_off", uniq_code: "wsx", amt_off: 4, dollar_or_percent: "$", status: 1)
+      coupon_5 = merchant_1.coupons.create!(name: "3_dollars_off", uniq_code: "rty", amt_off: 3, dollar_or_percent: "$", status: 1)
+      
+      visit "/merchants/#{merchant_1.id}/coupons/new"
+
+      fill_in(:name, with: "$20 off")
+      fill_in(:amt_off, with: "20")
+      fill_in(:dollar_or_percent, with:"$")
+      fill_in(:uniq_code, with:"sdf")
+      click_button "Create New Coupon"
+      expect(Coupon.all.count).to eq 5
+    end
   end
 end
